64ffa87bf85179d87c0b3b1af710f3e764ffd4aa
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-01-14 20:01:16 +0100

    Comments and minor

--- Day 3/spiral_memory.py
+++ Day 3/spiral_memory.py
@@ -1,5 +1,6 @@
 NUMBER = 361527
 
+# Get the ring and the distance from the center (1).
 ring = 0
 cont = 0
 for i in range(1,NUMBER,2):
@@ -8,11 +9,10 @@ for i in range(1,NUMBER,2):
         ring = i
         break
 
+# Get the ring just below ours.
 lower_ring = ring-2
-ring_values = []
-for i in range(lower_ring*lower_ring,ring*ring):
-    ring_values.append(i)
 
+# We can know the index in the list of our number
 index = NUMBER-lower_ring*lower_ring
 center = int(ring/2)
 
