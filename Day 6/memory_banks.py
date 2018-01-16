e8ea0c43e1b0b0e8a6465d6d7ef8a09ad1514de1
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-01-16 10:49:50 +0100

    Comments

--- Day 6/memory_banks.py
+++ Day 6/memory_banks.py
@@ -1,10 +1,11 @@
 BANKS = "2	8	8	5	4	2	3	1	5	5	1	2	15	13	5	14"
 
+# Format the list to int array.
 bank_list = BANKS.split('\t')
 for i in range(len(bank_list)):
     bank_list[i] = int(bank_list[i])
 
-
+# Keep track of the previous configs
 configuration = ' '.join(map(str,bank_list))
 previous_configurations = []
 redistributions = 0
